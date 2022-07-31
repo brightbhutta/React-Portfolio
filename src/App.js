78e22ba55c19b0cb26c79271ee7defffc8d07f03
Tanveer Bhutta <39510752+brightbhutta@users.noreply.github.com>
2022-07-31 15:03:36 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from "./PortfolioContainer/Home/Profile";
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+//import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
